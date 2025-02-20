c43beceb1d80f67013ea28f520d170b3b0b94c93
anstarikk <anseur88@outlook.fr>
2025-02-20 10:06:31 +0000

    3 task

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,18 +1,9 @@
-#include "main.h"
-#include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * main - Entry point, tests _islower function
- *
- * Return: Always 0
- */
-int main(void)
-{
-	char c1 = 'a';
-	char c2 = 'Z';
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
 
-	printf("%c: %d\n", c1, _islower(c1)); /* Doit afficher 1 */
-	printf("%c: %d\n", c2, _islower(c2)); /* Doit afficher 0 */
-
-	return (0);
-}
+#endif /* MAIN_H */
