92465199e433ac9b50377cebbf56e06d5daeaeb5
anstarikk <anseur88@outlook.fr>
2025-02-20 09:56:01 +0000

    2 task

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,7 +1,12 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-int _putchar(char c);
-void print_alphabet(void);
-
-#endif /* MAIN_H */
+/**
+ * main - Entry point, calls print_alphabet_x10
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
