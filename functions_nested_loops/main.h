0ca4cecf1a534e2eb59e5140f070b4ec1a114c23
anstarikk <anseur88@outlook.fr>
2025-02-20 10:04:34 +0000

    3 task

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,8 +1,18 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
+#include <stdio.h>
 
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
+/**
+ * main - Entry point, tests _islower function
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c1 = 'a';
+	char c2 = 'Z';
 
-#endif /* MAIN_H */
+	printf("%c: %d\n", c1, _islower(c1)); /* Doit afficher 1 */
+	printf("%c: %d\n", c2, _islower(c2)); /* Doit afficher 0 */
+
+	return (0);
+}
