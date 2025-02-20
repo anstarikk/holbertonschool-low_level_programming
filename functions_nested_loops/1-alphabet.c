8429b5135184dda69b9d2c485b1c8470eaf86516
anstarikk <anseur88@outlook.fr>
2025-02-20 09:47:47 +0000

    1 task

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,9 +1,17 @@
 #include "main.h"
 
+/**
+ * print_alphabet - Prints the alphabet in lowercase followed by a new line.
+ *
+ * Return: void
+ */
 void print_alphabet(void)
 {
-	char c;
-	for (c = 'a'; c <= 'z'; c++)
-		_putchar(c);
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
 	_putchar('\n');
 }
