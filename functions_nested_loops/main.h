54899788e642f3c015ebb1d919cd8a06df89ca9e
anstarikk <anseur88@outlook.fr>
2025-02-20 10:55:10 +0000

    7 task

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,12 +1,25 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
+#include <stdio.h>
 
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int n);
+/**
+ * main - Entry point, tests print_last_digit function
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n1 = 123;
+	int n2 = -456;
+	int n3 = 0;
 
-#endif /* MAIN_H */
+	printf("Last digit of %d: ", n1);
+	print_last_digit(n1);  /* Devrait afficher 3 */
+
+	printf("\nLast digit of %d: ", n2);
+	print_last_digit(n2);  /* Devrait afficher 6 */
+
+	printf("\nLast digit of %d: ", n3);
+	print_last_digit(n3);  /* Devrait afficher 0 */
+
+	return (0);
+}
