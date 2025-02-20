3e762b75c539c890832fdb9d5fb8809d2d286193
anstarikk <anseur88@outlook.fr>
2025-02-20 10:16:17 +0000

    5 task

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,10 +1,25 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
+#include <stdio.h>
 
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
+/**
+ * main - Entry point, tests print_sign function
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n1 = 10;
+	int n2 = 0;
+	int n3 = -5;
 
-#endif /* MAIN_H */
+	printf("%d: ", n1);
+	print_sign(n1);  /* Devrait afficher + et retourner 1 */
+
+	printf("\n%d: ", n2);
+	print_sign(n2);  /* Devrait afficher 0 et retourner 0 */
+
+	printf("\n%d: ", n3);
+	print_sign(n3);  /* Devrait afficher - et retourner -1 */
+
+	return (0);
+}
