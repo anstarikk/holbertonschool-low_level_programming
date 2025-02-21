98ffddda55f50e25faf05af093e372ea549c31fe
anstarikk <anseur88@outlook.fr>
2025-02-21 14:27:33 +0000

    modification du style betty

--- 11-print_to_98.c
+++ 11-print_to_98.c
@@ -1,32 +1,36 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
- * print_to_98 - prints all natural numbers from n to 98
- * @n: starting point
+ * print_to_98 - Prints all natural numbers from n to 98
+ * @n: The starting point
  *
- * Description: Prints all natural numbers from n to 98, followed by a newline.
+ * Description: This function prints all natural numbers from the given
+ * starting point 'n' up to 98, separated by commas and spaces.
+ * It prints in ascending order if n is less than or equal to 98,
+ * and in descending order if n is greater than 98.
  */
 void print_to_98(int n)
 {
 	int i;
 
 	if (n <= 98)
-	
+	{
 		for (i = n; i <= 98; i++)
 		{
 			printf("%d", i);
 			if (i != 98)
-			printf(", ");
-		
-	
+				printf(", ");
+		}
+	}
 	else
-
+	{
 		for (i = n; i >= 98; i--)
 		{
 			printf("%d", i);
 			if (i != 98)
 				printf(", ");
-		
 		}
+	}
 	printf("\n");
-
+}
