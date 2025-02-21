afb055275f148f8c8e54ae2536bbb08ac21747f6
anstarikk <anseur88@outlook.fr>
2025-02-21 14:50:07 +0000

    11 task

--- 11-print_to_98.c
+++ 11-print_to_98.c
@@ -4,7 +4,7 @@
 /**
  * print_to_98 - Prints all natural numbers from n to 98
  * @n: The starting point
- * 
+ *
  * Description: This function prints all natural numbers from the given
  * starting point 'n' up to 98, separated by commas and spaces.
  * It prints in ascending order if n is less than or equal to 98,
@@ -29,7 +29,7 @@ void print_to_98(int n)
 		{
 			printf("%d", i);
 			if (i != 98)
-				printf(", "); /* Suppression de l'espace en trop */
+				printf(", ");
 		}
 	}
 	printf("\n");
