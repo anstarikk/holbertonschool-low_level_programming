db6a925bd0a292d6efc601b41683e853d3f3aaf6
anstarikk <anseur88@outlook.fr>
2025-02-25 14:07:00 +0000

    1 task

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,8 +1,9 @@
-int _isdigit(int c)
-{
-if(c>='0'&&c<='9')
-return 1;
-else
-return 0;
-}
+#include <stdio.h>
+#include "main.h"  // Assurez-vous d'inclure main.h pour déclarer _isdigit
 
+int main(void)
+{
+    char c = '5';
+    printf("%c: %d\n", c, _isdigit(c));  // Appel à la fonction _isdigit
+    return 0;
+}
