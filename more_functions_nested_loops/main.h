07cfac7a5b2d6ad2b4ae833122767eb2edf15446
anstarikk <anseur88@outlook.fr>
2025-02-26 15:07:27 +0000

    3 task

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,8 +1,14 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-void print_numbers(void);
-int _putchar(char c);
+/**
+ * print_numbers - Prints numbers from 0 to 9 followed by a new line
+ */
+void print_numbers(void)
+{
+	int num;
 
-#endif /* MAIN_H */
+	for (num = 0; num <= 9; num++)
+		_putchar(num + '0');
+	_putchar('\n');
+}
 
