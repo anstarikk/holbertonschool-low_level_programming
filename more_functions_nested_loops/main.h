905094149430cee2f5a2a01fc658712adcecc608
anstarikk <anseur88@outlook.fr>
2025-02-26 14:57:07 +0000

    2 task

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,8 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _isupper(int c);
-int _isdigit(int c);
-
-#endif /* MAIN_H */
-
+/* Déclaration de la fonction mul */
+int mul(int a, int b);
+int mul(int a, int b);
+#endif
