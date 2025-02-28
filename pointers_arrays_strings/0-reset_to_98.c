5199cfa444d82ad4058231141a559740feb7e6ca
anstarikk <anstarikk>
2025-02-28 11:48:00 +0000

    0 task

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * reset_to_98 - Updates the value the pointer points to, to 98.
@@ -7,18 +7,4 @@
 void reset_to_98(int *n)
 {
 	*n = 98;
-}
-
-/**
- * main - Test the reset_to_98 function.
- * Return: Always 0.
- */
-int main(void)
-{
-	int n = 402;
-
-	printf("Before: %d\n", n);
-	reset_to_98(&n);
-	printf("After: %d\n", n);
-	return (0);
 }
