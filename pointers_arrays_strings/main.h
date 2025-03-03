35776bf8b8bca33b10036fee319e0283e005ec56
anstarikk <anseur88@outlook.fr>
2025-03-03 11:57:18 +0000

    6 task

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,7 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
+#include <stdio.h>
+
+/* Function prototypes */
+void _putchar(char c);
 void puts2(char *str);
+void rev_string(char *s);
+void print_rev(char *s);
+int _strlen(char *s);
 
 #endif /* MAIN_H */
