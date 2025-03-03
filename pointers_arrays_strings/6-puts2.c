fd4fa945d7c3123849bdbe5ed52373d69de99a93
anstarikk <anseur88@outlook.fr>
2025-03-03 12:01:47 +0000

    6 task

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -2,17 +2,22 @@
 
 /**
  * puts2 - Prints every other character of a string, starting with the first.
- * @str: The string to be printed.
+ * @str: The input string.
+ *
+ * Return: void
  */
 void puts2(char *str)
 {
-	int i;
+	int i = 0;
 
-	/* Loop through the string and print every other character */
-	for (i = 0; str[i] != '\0'; i += 2)
+	/* Loop through the string */
+	while (str[i] != '\0')
 	{
-		_putchar(str[i]);  /* Print the current character */
+		if (i % 2 == 0) /* Only print characters at even indices */
+		{
+			_putchar(str[i]);
+		}
+		i++;
 	}
-
-	_putchar('\n'); /* Print a new line after printing the string */
+	_putchar('\n'); /* Print a newline after the string */
 }
